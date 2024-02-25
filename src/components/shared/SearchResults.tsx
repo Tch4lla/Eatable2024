0f9791eb597a0869744fca60c90e367fa9417071
tchalla <sssillah1@gmail.com>
2024-02-25 23:11:56 +0900

    first commit

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,21 @@
+import Loader from './Loader';
+import GridPostList from './GridPostList';
+type SearchResultsProps = {
+  isSearchFetching: boolean;
+  searchedPost: any;
+};
+const SearchResults = ({
+  isSearchFetching,
+  searchedPost,
+}: SearchResultsProps) => {
+  if (isSearchFetching) return <Loader />;
+
+  if (searchedPost && searchedPost.documents.length > 0) {
+    return <GridPostList posts={searchedPost.documents} />;
+  }
+  return (
+    <p className="text-light-4 mt-10 text-center w-full">No Results Found</p>
+  );
+};
+
+export default SearchResults;
