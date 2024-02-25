c9020fd2a31850bf731b775776479a3f4ea3d719
tchalla <sssillah1@gmail.com>
2024-02-26 06:32:00 +0900

    resolved css conflict

--- src/_root/pages/Homepage/LandingPage1.tsx
+++ src/_root/pages/Homepage/LandingPage1.tsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
   return (
     <>
       <div className="max-w-7xl flex flex-wrap lg:w-1/2 lg:h-screen/2">
-        <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32 lg:mr-4 lg:ml-0 lg:w-full">
+        <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32 lg:mr-4 lg:ml-0">
           <svg
             className="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
             fill="currentColor"
