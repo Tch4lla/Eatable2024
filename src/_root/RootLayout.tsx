c82b44e532847af9165fa4ad88d286ebed0df8b3
tchalla <sssillah1@gmail.com>
2025-05-17 17:32:14 +0900

    update close modal

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -17,7 +17,7 @@ const RootLayout = () => {
     if (!user.id) {
       timeoutId = setTimeout(() => {
         setShowModal(true);
-      }, 90000);
+      }, 20000);
     }
 
     return () => {
@@ -48,7 +48,38 @@ const RootLayout = () => {
       <section className="flex flex-1 h-full">
         {showModal && (
           <div className="modal">
-            <div className="modal-content px-4">
+            <div className="modal-content px-4 relative">
+              <button
+                onClick={closeModal}
+                className="absolute right-4 top-2 rounded-sm opacity-70 hover:opacity-100 focus:outline-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="h-4 w-4"
+                >
+                  <line
+                    x1="18"
+                    y1="6"
+                    x2="6"
+                    y2="18"
+                  ></line>
+                  <line
+                    x1="6"
+                    y1="6"
+                    x2="18"
+                    y2="18"
+                  ></line>
+                </svg>
+                <span className="sr-only">Close</span>
+              </button>
               <div className="modal-left">
                 <img
                   src="/assets/images/heroPhoto.JPG"
